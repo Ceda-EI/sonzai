dedf976c95a05cc019065550e64f6f2564520815
Ceda EI <ceda_ei@webionite.com>
2020-03-21 20:22:08 +0530

    Add transformer to transform Date.

--- configureStore.js
+++ configureStore.js
@@ -1,11 +1,20 @@
 import { createStore } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, createTransform } from "redux-persist";
 import AsyncStorage from "@react-native-community/async-storage";
 import rootReducer from "./reducers";
+
+function decode(toRehydrate) {
+	return JSON.parse(toRehydrate, (_, value) => {
+		if (typeof value === "string" && value.match(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/))
+			return new Date(value);
+		return value;
+	});
+}
 
 const persistConfig = {
 	key: "root",
 	storage: AsyncStorage,
+	transforms: [createTransform(JSON.stringify, decode)],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
